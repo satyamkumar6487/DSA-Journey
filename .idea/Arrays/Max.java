775cc1917f05e2f558e7df42ec04d3faad162505
satyamkumar6487 <jsatyamkumar597@gmail.com>
2026-02-09 18:34:30 +0530

    solve majority of n\3 problem and stock problem

---
+++ .idea/Arrays/Max.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Max {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 2, 3, 1, 3, 2, 1, 1};
+
+        ArrayList<Integer> result = findmajority(arr);
+
+        System.out.println(result);
+    }
+
+    static ArrayList<Integer> findmajority(int[] arr) {
+
+        int cand1 = 0, cand2 = 0;
+        int count1 = 0, count2 = 0;
+
+        // 1 Candidate selection
+        for (int num : arr) {
+
+            if (num == cand1)
+                count1++;
+
+            else if (num == cand2)
+                count2++;
+
+            else if (count1 == 0) {
+                cand1 = num;
+                count1 = 1;
+
+            } else if (count2 == 0) {
+                cand2 = num;
+                count2 = 1;
+
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // 2Verification
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : arr) {
+            if (num == cand1) count1++;
+            else if (num == cand2) count2++;
+        }
+
+        // 3⃣ Result
+        ArrayList<Integer> res = new ArrayList<>();
+
+        if (count1 > arr.length / 3)
+            res.add(cand1);
+        if (count2 > arr.length / 3)
+            res.add(cand2);
+
+        Collections.sort(res);
+        return res;
+    }
+}
---
+++ .idea/Arrays/Stock.java
@@ -0,0 +1,28 @@
+public  class Stock {
+
+    public static void main(String[] args) {
+
+        int[] price = {100, 180, 260, 310, 40, 535, 695};
+
+        System.out.println(maxprofit(price));
+
+    }
+
+    static int maxprofit(int[] price) {
+        int profit = 0 ;
+
+        for (int i = 1  ; i <price.length ; i ++){
+
+            if (price[i] > price[i -1 ]){
+
+                profit += price[i] - price[i-1];
+            }
+
+
+        }
+        return profit;
+
+
+    }
+
+}
