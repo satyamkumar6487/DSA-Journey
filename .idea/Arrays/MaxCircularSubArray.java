f4ef730f90ee80e18886535bf484f82578fbb0d2
satyamkumar6487 <jsatyamkumar597@gmail.com>
2026-02-12 18:55:41 +0530

    solve maxcircularArray Problem

---
+++ .idea/Arrays/MaxCircularSubArray.java
@@ -0,0 +1,47 @@
+public class MaxCircularSubArray {
+    public static  void main (String []args){
+
+        int [] arr = {8, -8, 9, -9, 10, -11, 12};
+
+        System.out.println(circularArray(arr));
+    }
+
+    static int circularArray(int []arr){
+
+        int n = arr.length;
+
+        int currentMax = arr[0];
+        int currentMin = arr[0];
+        int Totalsum = arr[0];
+        int maxnormal= arr[0];
+
+        int minsub = arr[0];
+
+        for (int i = 1 ; i < n ; i ++){
+
+            Totalsum += arr[i];
+
+            currentMax = Math.max(arr[i] , currentMax + arr[i]);
+
+
+            maxnormal = Math.max(maxnormal , currentMax);
+
+            currentMin = Math.min(arr[i] , currentMin + arr[i]);
+            minsub = Math.min(minsub , currentMin);
+
+
+        }
+
+        if (maxnormal < 0){
+            return maxnormal;
+        }
+
+        int maxcircular = Totalsum - minsub;
+
+        return  Math.max(maxnormal , maxcircular);
+
+
+    }
+
+}
+
